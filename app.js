50dece21a69fa4519107a84cdb476bd302fb5442
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 22:27:31 +0700

    js lab 52

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+// - ให้สร้างฟังก์ชันเพื่อตรวจสอบ object ว่าเป็น object ว่างหรือไม่ 
+// - ถ้า object มี property อย่างน้อยหนึ่งแสดงว่าไม่ใช่ object ว่าง
+
+function isEmptyObject(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+let obj1 = {};
+let obj2 = { a: "aa"};
+
+console.log(isEmptyObject(obj1))
+console.log(isEmptyObject(obj2))
+
+
+
+
